22d54662bbff8495f85a63020270f77cff5ca921
Avery Ginder <aginder@student.neumont.edu>
2024-04-30 22:46:30 -0600

    new changes from the create body manager assignment

--- game/src/main.c
+++ game/src/main.c
@@ -1,7 +1,6 @@
-#include "body.h"
-#include "raylib.h"
 #include "mathf.h"
 #include "raymath.h"
+#include "world.h"
 
 #include <stdlib.h>
 #include <assert.h>
@@ -13,26 +12,19 @@ int main(void)
     InitWindow(1280, 720, "Physics Engine");
     SetTargetFPS(60);
 
-    // Body* bodies = new Body[] (thats what the next line does)
-    Body* bodies = (Body*)malloc(sizeof(Body) * MAX_BODIES); //malloc allocates bytes
-    assert(bodies); // assert(bodies) also does the same thing basically
-
-    int bodyCount = 0;
-
     // game loop
     while (!WindowShouldClose())
     {
         // update
-        float dt = GetFrameTime(); 
+        float dt = GetFrameTime();
         float fps = (float)GetFPS();
 
-        Vector2 position = GetMousePosition();
+        Vector2 mousePosition = GetMousePosition();
 
         if (IsMouseButtonDown(0))
         {
-            bodies[bodyCount].position = position;
-            bodies[bodyCount].velocity = CreateVector2(GetRandomFloatValue(-5, 5), GetRandomFloatValue(-5, 5));
-            bodyCount++;
+            // creates new body when mouse is clicked using CreateBody method
+            CreateBody(mousePosition);
         }
 
         // renders and draws stuff
@@ -43,20 +35,22 @@ int main(void)
         DrawText(TextFormat("FPS: %.2f (%.2f ms)", fps, 1000 / fps), 10, 10, 20, LIME);
         DrawText(TextFormat("FRAME: %.4f", dt), 10, 30, 20, LIME);
 
-        DrawCircle((int)position.x, (int)position.y, 20, RED);
+        DrawCircle((int)mousePosition.x, (int)mousePosition.y, 20, RED);
 
-        for (int i = 0; i < bodyCount; i++)
+        // iterates through bodies and draws them
+        Body* currentBody = bodies;
+        while (currentBody != NULL)
         {
-            bodies[i].position = Vector2Add(bodies[i].position, bodies[i].velocity);
-            DrawCircle((int)bodies[i].position.x, (int)bodies[i].position.y, 10, RED);
+            currentBody->position = Vector2Add(currentBody->position, currentBody->velocity);
+            DrawCircle((int)currentBody->position.x, (int)currentBody->position.y, 10, RED);
+            currentBody = currentBody->next;
         }
-
 
         EndDrawing();
     }
 
     CloseWindow();
-    free(bodies); // have to free (delete) bodies
+    // bodies no longer need to be deleted here
 
     return 0;
 }
--- game/world.c
+++ game/world.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <assert.h>
 #include "world.h"
 
 Body* bodies = NULL;
@@ -5,10 +7,54 @@ int bodyCount = 0;
 
 Body* CreateBody()
 {
-	return NULL;
+    // malloc allocates memory for the new body
+    Body* newBody = (Body*)malloc(sizeof(Body));
+
+    if (newBody == NULL) 
+    {
+        return NULL;
+    }
+
+    // initializes prev in newBody to to NULL, and next to the head of the list
+    newBody->prev = NULL;
+    newBody->next = bodies; 
+
+    // checks if list is empty, if not, updates prev in newBody
+    if (bodies != NULL) 
+    {
+        bodies->prev = newBody;
+    }
+
+    // updates head of list with newBody
+    bodies = newBody;
+
+    bodyCount++;
+    return newBody;
 }
 
 void DestroyBody(Body* body)
 {
+    // makes sure the body given is not null
+    assert(body != NULL);
 
+    // If 'prev' is not NULL, set 'prev->next' to 'body->next'
+    if (body->prev != NULL) {
+        body->prev->next = body->next;
+    }
+
+    // following two if statements essentially just check to see if the body is the first or last in the list, and then it makes
+    // the necessary adjustments
+
+    if (body->next != NULL) {
+        body->next->prev = body->prev;
+    }
+
+    if (bodies == body) {
+        bodies = body->next;
+    }
+
+    bodyCount--;
+
+    // deletes the body and frees up the space in memory it was taking up
+    free(body);
 }
